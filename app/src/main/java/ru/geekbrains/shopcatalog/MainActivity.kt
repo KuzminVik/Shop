3549a6bcad4788df48037b7783ac9873bc5d090e
KuzminVik <viksimurg@gmail.com>
2021-05-05 20:48:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/shopcatalog/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.geekbrains.shopcatalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.shopcatalog.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
